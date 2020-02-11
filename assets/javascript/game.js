dc842f767b6ec7f8c9ea74b762a997ecebe94ca5
Glenn Dixon <glenndixon19@gmail.com>
2020-02-11 04:10:02 -0600

    changed variables to let and const

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
 // Global Var
 //=====================================================================
 // Arrays and variables needed for holding data
-var sitcoms = ["waffles", "parks", "ann", "little sebastian", "ben"];
-var secretWord = "";
-var lettersinWord = [];
-var numBlanks = 0;
-var blanksAndSucesses = [];
-var wrongGuesses = [];
+const sitcoms = ["waffles", "parks", "ann", "little sebastian", "ben"];
+let secretWord = "";
+let lettersinWord = [];
+const numBlanks = 0;
+let blanksAndSucesses = [];
+let wrongGuesses = [];
 
 // Game Counters
-var winCount = 0;
-var lossCount = 0;
-var guessesLeft = 10;
+let winCount = 0;
+let lossCount = 0;
+const guessesLeft = 10;
