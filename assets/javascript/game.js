3036251a1c6d7765a994388f64d3822578c79d65
Glenn Dixon <glenndixon19@gmail.com>
2020-02-11 03:53:09 -0600

    created global veriables

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Global Var
+//=====================================================================
+// Arrays and variables needed for holding data
+var sitcoms = ["waffles", "parks", "ann", "little sebastian", "ben"];
+var secretWord = "";
+var lettersinWord = [];
+var numBlanks = 0;
+var blanksAndSucesses = [];
+var wrongGuesses = [];
+
+// Game Counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 10;
